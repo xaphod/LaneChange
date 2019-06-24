02e39f99d2c1e2f580619728776db38017853de6
Tim Carr <xaphod@gmail.com>
2019-06-24 15:19:28 -0400

    Improve terms popup

--- app/utils/termsPopup.js
+++ app/utils/termsPopup.js
@@ -1,15 +1,11 @@
 import { Alert } from 'react-native';
 import { openTerms, openPrivacy } from 'app/utils/constants';
 
-export default showTermsAlert = (onPress) => {
+const showInnerAlert = (onPress) => {
   Alert.alert(
-    'Almost There',
-    'When you use this app, it uploads data to our servers. We take your security & privacy seriously. By continuing to use this app you are agreeing to the Terms and Conditions and the Privacy Policy.',
+    'Read',
+    'Please take a moment to read the following.',
     [
-      {
-        text: 'Continue',
-        onPress: () => onPress(),
-      },
       {
         text: 'Terms and Conditions',
         onPress: () => openTerms(),
@@ -17,6 +13,29 @@ export default showTermsAlert = (onPress) => {
       {
         text: 'Privacy Policy',
         onPress: () => openPrivacy(),
+      },
+      {
+        text: 'Accept and continue',
+        onPress: () => onPress(),
+        style: 'cancel',
+      },
+    ],
+  );
+};
+
+export default showTermsAlert = (onPress) => {
+  Alert.alert(
+    'Almost There',
+    'When you use this app, it uploads data to our servers. We take your security & privacy seriously. By continuing to use this app you are agreeing to the Terms and Conditions and the Privacy Policy which can be updated at any time.',
+    [
+      {
+        text: 'Read...',
+        onPress: () => showInnerAlert(onPress),
+      },
+      {
+        text: 'Accept and continue',
+        onPress: () => onPress(),
+        style: 'cancel',
       },
     ],
   );
